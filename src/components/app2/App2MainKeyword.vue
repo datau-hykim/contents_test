cbb94fe826a63de82286ad1bcab630cd0a21c6ad
datau-hykim <hykim@datau.co.kr>
2022-05-02 09:58:34 +0900

    STYLE modify

--- src/components/app2/App2MainKeyword.vue
+++ src/components/app2/App2MainKeyword.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="App2Mainkeyword">
     <div class="keyword-title">주요피싱키워드</div>
-    <div class="keyword-contents">
-      <div class="keyword-contents-box">
-        <div class="keyword-contents-text">{{ keywords.Title && `${keywords.OrderNum}. ${keywords.Title}` }}</div>
-        <div
-          class="keyword-contents-img"
-          v-if="keywords.Arrow"
-          :class="(keywords.Arrow === 'hot' || keywords.Arrow === 'new') && 'box'"
-        >
-          <img
-            class="keyword-contents-image-size"
-            :src="require(`@/assets/app2-mainkeyword-ticker_${keywords.Arrow}.png`)"
-          />
-        </div>
+    <div class="keyword-contents-box">
+      <div class="keyword-contents-text">{{ keywords.Title && `${keywords.OrderNum}. ${keywords.Title}` }}</div>
+      <div
+        class="keyword-contents-img"
+        v-if="keywords.Arrow"
+        :class="(keywords.Arrow === 'hot' || keywords.Arrow === 'new') && 'box'"
+      >
+        <img
+          class="keyword-contents-image-size"
+          :src="require(`@/assets/app2-mainkeyword-ticker_${keywords.Arrow}.png`)"
+        />
       </div>
     </div>
   </div>
