32e9c024d7e5a3149596eedcf6375e92489a13bb
datau-hykim <hykim@datau.co.kr>
2022-05-02 12:36:24 +0900

    feat btn_link

---
+++ src/components/app2/App2BtnLink.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="btn_link">
+    <img class="btn_link_top shake-bottom" src="@/assets/btn_icon.png" @click="goPage('/')" />
+    <img class="btn_link_bottom" src="@/assets/btn_text.png" @click="goPage('/')" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+<style scoped>
+@import "@/styles/App2BtnLink.scss";
+</style>
